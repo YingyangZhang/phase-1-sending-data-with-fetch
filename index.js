c5a4c0ab7fa7709805bece83a94534333e6794bf
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-09-02 20:46:29 -0400

    commit tree 1e3d6af437edbd465039df3992801f2f5df6269e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+function submitData() {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        })
+    })
+    .then(res => res.json())
+    .then(json => document.body.innerHTML = json.id)
+    .catch(error => document.body.innerHTML = error.message)
+}
